4bdc0b697fa8a768c184b348a9af3601eb52ff58
Hugo <phugofj@gmail.com>
2022-08-07 20:31:21 -0300

    Consulta de user com tratamento de erro.

--- api-mini-blog/src/main/java/br/com/techugo/apiminiblog/resources/UserResource.java
+++ api-mini-blog/src/main/java/br/com/techugo/apiminiblog/resources/UserResource.java
@@ -5,11 +5,8 @@ import br.com.techugo.apiminiblog.domain.User;
 import br.com.techugo.apiminiblog.dto.UserDTO;
 import br.com.techugo.apiminiblog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,5 +25,10 @@ public class UserResource {
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return listDto;
         //return this.service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable String id){
+        return this.service.findById(id);
     }
 }
--- api-mini-blog/src/main/java/br/com/techugo/apiminiblog/services/UserService.java
+++ api-mini-blog/src/main/java/br/com/techugo/apiminiblog/services/UserService.java
@@ -2,10 +2,12 @@ package br.com.techugo.apiminiblog.services;
 
 import br.com.techugo.apiminiblog.domain.User;
 import br.com.techugo.apiminiblog.repository.UserRepository;
+import br.com.techugo.apiminiblog.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        return repo.findById(id)
+                    .orElseThrow(() -> new ObjectNotFoundException("Usuário não existe"));
     }
 }
