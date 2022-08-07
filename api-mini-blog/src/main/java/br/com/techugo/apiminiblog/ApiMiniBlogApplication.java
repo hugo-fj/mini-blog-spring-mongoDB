d1f6c59e2b09ce7302ca3c0a001fe1ba87ecf051
Hugo <phugofj@gmail.com>
2022-08-07 13:30:29 -0300

    Criação de projeto

---
+++ api-mini-blog/src/main/java/br/com/techugo/apiminiblog/ApiMiniBlogApplication.java
@@ -0,0 +1,13 @@
+package br.com.techugo.apiminiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMiniBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMiniBlogApplication.class, args);
+	}
+
+}
